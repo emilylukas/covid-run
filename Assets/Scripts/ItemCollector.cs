e21b06b8a4377688f83fc0aac5740b06ab9c177e
Ivy <Ivy@Ivys-MacBook-Air.local>
2022-01-15 22:30:16 -0800

    Added point counter

--- Assets/Scripts/ItemCollector.cs
+++ Assets/Scripts/ItemCollector.cs
@@ -1,16 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class ItemCollector : MonoBehaviour
 {
     int vaccine = 0;
+
+    [SerializeField] Text vaccineText;
+
+
     private void OnTriggerEnter(Collider other){
         if (other.gameObject.CompareTag("Vaccine")) 
         {
             Destroy(other.gameObject);
             vaccine++;
-            Debug.Log("Vaccine points: " + vaccine);
+            vaccineText.text = "Points: " + vaccine;
         }
     }
 }
